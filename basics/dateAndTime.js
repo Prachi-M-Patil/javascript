e12080e3fd3e28a9f6e601a20e05aac1d661b32d
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2024-09-04 13:42:23 +0000

    date and time

---
+++ basics/dateAndTime.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+let myCreatedDate = new Date("25-10-2024");
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
