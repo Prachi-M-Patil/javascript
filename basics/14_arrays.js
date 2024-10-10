8a58da80f00266196dcd62cc52f68838cbd9bf09
Prachi-M-Patil <127610283+Prachi-M-Patil@users.noreply.github.com>
2024-10-10 13:27:55 +0000

    arrays

---
+++ basics/14_arrays.js
@@ -0,0 +1,13 @@
+const myArr = [0,1,2,3,4]
+
+console.log(myArr[0]);
+
+//array methhods
+myArr.push(6);
+console.log(myArr);
+myArr.pop();
+console.log(myArr);
+
+myArr.unshift(9);
+myArr.shift()
+console.log(myArr);
